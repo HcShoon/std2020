dd8ef430de9337fc935efa67b8fe8a87fb174d81
Hcshoon <daybee01@naver.com>
2020-04-10 14:35:03 +0900

    주소록 만들기

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
 노드 js로 게시판 만들기
 
 ***2020.04.09 - node-js 시작
-   + node js 설치
-     - index.js 로 Hello World! 출력
-   + ejs 설치
-     - url 지정후 호출
-     - 파라미터 전송 및 호출
+  + node js 설치
+    - index.js 로 Hello World! 출력
+
+  + ejs 설치
+    - url 지정후 호출
+    - 파라미터 전송 및 호출
+
+***2020.04.10 - 주소록 만들기 (nodeJs - mongdb 연결)
+  + ejs express mongdb package 설치
+    - npm install --save ejs express mongoose
+    - mongo db 회원가입 - 무료 회원 500mb 사용가능
+    - mongo db 환경변수에 MONGO_DB 지정 - mongo db 사이트에서 connenction url 할당
+    - mongi db connect 확인
+
+  + body-parser package 설치
+    - npm install --save body-parser 
+    - bodyParser를 통해 리턴데이터형 json으로 지정
+    - bodyParser.urlencoded 통해 알고리즘 분석 설정
+
+  + method-override package 설치(이 package는 query로 method 값을 받아서 request의 HTTP method를 바꿔주는 역할을 합니다.)
+    - npm install --save method-override
+    - update과 destroy는 HTTP Methods 중 put과 delete을 사용하는데, 대부분의 브라우저의 form은 get과 post 만을 허용하고 나머지는 허용하지 않습니다. 
+    - 브라우저에서 허용하진 않지만 나중에 API로 연결할 때는 문제가 없기 때문에 HTTP를 올바르게 사용하는 법을 익히는 것이 더 중요합니다. 
+    - 지금은 일단 method override라는  package를 설치하여 이를 우회하도록하겠습니다.
--- index.js
+++ index.js
@@ -1,28 +1,70 @@
-﻿var express = require('express'); // 설치한 express module을 불러와서 변수(express)에 담습니다.
-var app = express(); //express를 실행하여 app object를 초기화 합니다.
+// index.js
 
-// app.get('/', function(req, res) { // '/' 위치에 'get'요청을 받는 경우,
-//   res.send('Hello World!'); // "Hello World!"를 보냅니다.
-// });
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser'); // 1
+var app = express();
 
-//app.use(express.static(__dirname + '/public')); // 폴더 지정
+// DB setting ...
+mongoose.set('useNewUrlParser', true);    // 1 mongoose의 몇몇 글로벌 설정을 해 주는 부분입니다. 
+mongoose.set('useFindAndModify', false);  // 1 저 부분이 바뀔 일은 왠만하면 없기 때문에 그냥 항상 저렇게 설정하고 쓰시면 됩니다. .
+mongoose.set('useCreateIndex', true);     // 1 이 부분이 빠지게 되면 서버 실행시 경고를 냅니다
+mongoose.set('useUnifiedTopology', true); // 1
+mongoose.connect(process.env.MONGO_DB); // 2
 
-app.set('view engine','ejs'); // 1  ejs를 사용하기 위해서 express의 view engine에 ejs를 set하는 코드입니다.
-app.use(express.static(__dirname + '/public'));
+var db = mongoose.connection; 
 
-app.get('/hello', function(req,res){ // 2 query를 통해서 이름을 받는 코드입니다. 모든 query들은 req.query에 저장됩니다.
-  res.render('hello', {name:req.query.nameQuery});
+//4 db가 성공적으로 연결된 경우 "DB connected"를 출력합니다.
+db.once('open', function(){
+    console.log('DB connected');
+  });
+//5 db연결중 에러가 있는 경우 "DB ERROR : " 와 에러를 출력합니다.
+db.on('error', function(err){
+    console.log('DB ERROR : ', err);
 });
 
-app.get('/hello/:nameParam', function(req,res){ // 3 route parameter를 통해 이름을 받는 코드입니다. 
-                                                // 콜론(:)으로 시작되는 route은 해당 부분에 입력되는 
-                                                // route의 텍스트가 req.params에 저장됩니다.
-                                                // 예를들어 /hello/Kim을 입력하면 "/hello/:nameParam"에 의해 
-                                                // 세미콜론이 있는 route의 2번째 부분 즉, Kim이 req.params.nameParam으로 저장됩니다.
-  res.render('hello', {name:req.params.nameParam});
+// Other settings
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.json()); // 2 json 형태로 받음 설정
+app.use(bodyParser.urlencoded({extended:true})); // 3 urlencoded data를 extended 알고리즘을 사용해서 분석한다는 설정
+
+// DB schema // 4 스키마 정의
+var contactSchema = mongoose.Schema({
+  name:{type:String, required:true, unique:true},
+  email:{type:String},
+  phone:{type:String}
 });
 
-var port = 3000; // 사용할 포트 번호를 port 변수에 넣습니다. 
-app.listen(port, function(){ // port변수를 이용하여 3000번 포트에 node.js 서버를 연결합니다.
-  console.log('server on! http://localhost:'+port); //서버가 실행되면 콘솔창에 표시될 메세지입니다.
+var Contact = mongoose.model('contact', contactSchema); // 5 정의된 스키마 mongo db에 저장
+
+// Routes
+// Home // 6
+app.get('/', function(req, res){
+  res.redirect('/contacts');
+});
+// Contacts - Index // 7
+app.get('/contacts', function(req, res){
+  Contact.find({}, function(err, contacts){
+    console.log(contacts);
+    if(err) return res.json(err);
+    res.render('contacts/index', {contactList:contacts});
+  });
+});
+// Contacts - New // 8
+app.get('/contacts/new', function(req, res){
+  res.render('contacts/new');
+});
+// Contacts - create // 9
+app.post('/contacts', function(req, res){
+  Contact.create(req.body, function(err, contact){
+    if(err) return res.json(err);
+    res.redirect('/contacts');
+  });
+});
+
+// Port setting
+var port = 3000;
+app.listen(port, function(){
+  console.log('server on! http://localhost:'+port);
 });
